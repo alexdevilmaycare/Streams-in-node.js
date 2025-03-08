210d7248631ac07b2606499782597a7d24f01e56
Alex Levkovets <sashalevk111aaa@gmail.com>
2025-03-08 14:49:43 +0300

    Added Guardian Class
    Added the Guardian Class which is the extension of Transform stream_classes

---
+++ stream_classes/guardian.js
@@ -0,0 +1,12 @@
+import Transform from "node:stream"; 
+
+export class Guardian extends Transform {
+    constructor (options) {
+        super(options);
+    } 
+    _transform (chunk, encoding, callback) {
+        console.log(chunk) 
+        this.push(number); 
+        callback(); 
+    }
+}
